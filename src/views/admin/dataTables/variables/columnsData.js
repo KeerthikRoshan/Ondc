a0827ad40a93e8f26ac8897e6921bc48104b832e
sesankrishnan <sesankrish07@gmail.com>
2024-02-07 10:05:21 +0530

    hello

---
+++ src/views/admin/dataTables/variables/columnsData.js
@@ -0,0 +1,88 @@
+export const columnsDataDevelopment = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "TECH",
+    accessor: "tech",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+  {
+    Header: "PROGRESS",
+    accessor: "progress",
+  },
+];
+
+export const columnsDataCheck = [
+  {
+    Header: "Company Name",
+    accessor: "Company Name", // Updated accessor name
+  },
+  {
+    Header: "Type",
+    accessor: "Type", // Updated accessor name
+  },
+  {
+    Header: "View",
+    accessor: "View", // Updated accessor name
+  },
+  {
+    Header: "DATE",
+    accessor: "DATE",
+  },
+];
+
+export const columnsDataColumns = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "PROGRESS",
+    accessor: "progress",
+  },
+  {
+    Header: "QUANTITY",
+    accessor: "quantity",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+];
+
+export const columnsDataComplex = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "TYPE",
+    accessor: "type",
+  },
+  {
+    Header: "LOCATION",
+    accessor: "location",
+  },
+  {
+    Header: "PROGRESS",
+    accessor: "progress",
+  },
+  
+  {
+    Header: "VIEW",
+    accessor: "view",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+  {
+    Header: "STATUS",
+    accessor: "status",
+  },
+];
